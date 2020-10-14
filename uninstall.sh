7f49c9d950807bb571e85009444c945f9b16e758
Jorge Ivan <59676765+Leviatansan21@users.noreply.github.com>
2020-10-14 14:27:52 -0500

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+echo "Deleting Componets of Windows"
+echo "Please Wait..."
+echo ""
+sleep 5s
+rm  /usr/share/gnome-background-properties/Plus98.xml
+rm  /usr/share/backgrounds/Plus98
+rm  /usr/share/fonts/Win98
+rm  /usr/share/icons/"Dangerous Creatures (256 color)"
+rm  /usr/share/icons/"Inside your Computer (high color)"
+rm  /usr/share/icons/"Jungle (256 color)"
+rm  /usr/share/icons/"Mystery (high color)"
+rm  /usr/share/icons/"BaseBall (256 color)"
+rm  /usr/share/icons/"More Windows (high color)"
+rm  /usr/share/icons/"Leonardo da Vinci (256 color)"
+rm  /usr/share/icons/"Windows 98 (256 color)"
+rm  /usr/share/icons/"Sports (256 color)"
+rm  /usr/share/icons/"Nature (high color)"
+rm  /usr/share/icons/"Science (256 color)"
+rm  /usr/share/icons/"Underwater (high color)"
+rm  /usr/share/icons/"The Golden Era (high color)"
+rm  /usr/share/icons/"Travel (high color)"
+rm  /usr/share/icons/"The 60's USA (256 color)"
+rm  /usr/share/themes/"Dangerous Creatures (256 color)"
+rm  /usr/share/themes/"Inside your Computer (high color)"
+rm  /usr/share/themes/"Jungle (256 color)"
+rm  /usr/share/themes/"Mystery (high color)"
+rm  /usr/share/themes/"BaseBall (256 color)"
+rm  /usr/share/themes/"More Windows (high color)"
+rm  /usr/share/themes/"Leonardo da Vinci (256 color)"
+rm  /usr/share/themes/"Windows 98 (256 color)"
+rm  /usr/share/themes/"Sports (256 color)"
+rm  /usr/share/themes/"Nature (high color)"
+rm  /usr/share/themes/"Science (256 color)"
+rm  /usr/share/themes/"Underwater (high color)"
+rm  /usr/share/themes/"The Golden Era (high color)"
+rm  /usr/share/themes/"Travel (high color)"
+rm  /usr/share/themes/"The 60's USA (256 color)"
+rm  /usr/share/sounds/"Dangerous Creatures (256 color)"
+rm  /usr/share/sounds/"Inside your Computer (high color)"
+rm  /usr/share/sounds/"Jungle (256 color)"
+rm  /usr/share/sounds/"Mystery (high color)"
+rm  /usr/share/sounds/"BaseBall (256 color)"
+rm  /usr/share/sounds/"More Windows (high color)"
+rm  /usr/share/sounds/"Leonardo da Vinci (256 color)"
+rm  /usr/share/sounds/"Windows 98 (256 color)"
+rm  /usr/share/sounds/"Sports (256 color)"
+rm  /usr/share/sounds/"Nature (high color)"
+rm  /usr/share/sounds/"Science (256 color)"
+rm  /usr/share/sounds/"Underwater (high color)"
+rm  /usr/share/sounds/"The Golden Era (high color)"
+rm  /usr/share/sounds/"Travel (high color)"
+rm  /usr/share/sounds/"The 60's USA (256 color)"
+rm  /usr/share/icons/Cathy
+rm  /usr/share/icons/Doonesbury
+rm  /usr/share/icons/Fashion
+rm  /usr/share/icons/FoxTrot
+rm  /usr/share/icons/"Corbis Photography (high color)"
+rm  /usr/share/icons/"Horror Channel (high color)"
+rm  /usr/share/icons/"PhotoDisc (high color)"
+rm  /usr/share/icons/"Rock-n-Roll (high color)"
+rm  /usr/share/icons/"Sci-Fi (high color)"
+rm  /usr/share/icons/"Geometry (high color)"
+rm  /usr/share/icons/Jazz
+rm  /usr/share/icons/Peanuts
+rm  /usr/share/icons/Garfield
+rm  /usr/share/themes/Cathy
+rm  /usr/share/themes/Doonesbury
+rm  /usr/share/themes/Fashion
+rm  /usr/share/themes/FoxTrot
+rm  /usr/share/themes/"Corbis Photography (high color)"
+rm  /usr/share/themes/"Horror Channel (high color)"
+rm  /usr/share/themes/"PhotoDisc (high color)"
+rm  /usr/share/themes/"Rock-n-Roll (high color)"
+rm  /usr/share/themes/"Sci-Fi (high color)"
+rm  /usr/share/themes/"Geometry (high color)"
+rm  /usr/share/themes/Jazz
+rm  /usr/share/themes/Peanuts
+rm  /usr/share/themes/Garfield
+rm  /usr/share/sounds/Cathy
+rm  /usr/share/sounds/Doonesbury
+rm  /usr/share/sounds/Fashion
+rm  /usr/share/sounds/FoxTrot
+rm  /usr/share/sounds/"Corbis Photography (high color)"
+rm  /usr/share/sounds/"Horror Channel (high color)"
+rm  /usr/share/sounds/"PhotoDisc (high color)"
+rm  /usr/share/sounds/"Rock-n-Roll (high color)"
+rm  /usr/share/sounds/"Sci-Fi (high color)"
+rm  /usr/share/sounds/"Geometry (high color)"
+rm  /usr/share/sounds/Jazz
+rm  /usr/share/sounds/Peanuts
+rm  /usr/share/sounds/Garfield
+echo ""
+echo "Thank You for Use Plus98 of leviatan_san"
